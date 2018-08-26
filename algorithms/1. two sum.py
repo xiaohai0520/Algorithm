89f84991fab73ccf59ae77da10e5835cb4b16fc5
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-08-26 19:56:01 -0400

    Update 1. two sum.py

--- algorithms/1. two sum.py
+++ algorithms/1. two sum.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+#         dic = {}
+#         for i in range(len(nums)):
+#             if nums[i] in dic:
+#                 return [dic[nums[i]], i]
+#             else:
+#                 dic[target - nums[i]] = i
+        dic = {}        
+        for i,num in enumerate(nums):
+            if num in dic:
+                return [dic[num],i]
+            else:
+                dic[target - num] = i
