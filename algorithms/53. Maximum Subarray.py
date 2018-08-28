c1fe5bea2de06d0b4a65c1aa1a1ba84feab1c4d8
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-08-27 20:32:05 -0400

    Update 53. Maximum Subarray.py

--- algorithms/53. Maximum Subarray.py
+++ algorithms/53. Maximum Subarray.py
@@ -30,8 +30,31 @@ class Solution:
             cursum = max(cursum+nums[i],nums[i])
             dp[i] = max(cursum,dp[i-1])
         return dp[-1]
+           
+        
+        #divide and conquer
+        return self.helper(nums,0,len(nums)-1)
+        
+        
+    def helper(self,nums,left,right):
+        if left == right:
+            return nums[left]
+        mid = (left + right) // 2
+        
+        suml = leftsum = nums[mid]
+        for i in range(mid -1,left -1,-1):
+            suml += nums[i]
+            leftsum = max(leftsum,suml)
             
-            
+        sumr = rightsum = nums[mid+1]
+        for i in range(mid+2,right+1):
+            sumr += nums[i]
+            rightsum = max(rightsum,sumr)
+        
+        leftmax = self.helper(nums,left,mid)
+        rightmax = self.helper(nums,mid+1,right)
+        
+        return max(leftmax,rightmax,(leftsum + rightsum))      
             
             
         
