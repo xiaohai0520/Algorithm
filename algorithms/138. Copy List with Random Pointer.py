b015bb1cf4397be20d571e2db3a63b69effc9267
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-02 22:46:41 -0400

    Create 138. Copy List with Random Pointer.py

---
+++ algorithms/138. Copy List with Random Pointer.py
@@ -0,0 +1,31 @@
+#use a dictionary to save the new node at the same position 
+#then try to link them
+
+# Definition for singly-linked list with a random pointer.
+# class RandomListNode(object):
+#     def __init__(self, x):
+#         self.label = x
+#         self.next = None
+#         self.random = None
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        if not head:
+            return None
+        cur, dic = head, {}
+        while cur:
+            dic[cur] = RandomListNode(cur.label)
+            cur = cur.next
+        cur = head
+        while cur:
+            if cur.random:
+                dic[cur].random = dic[cur.random]
+            if cur.next:
+                dic[cur].next = dic[cur.next]
+            cur = cur.next
+        return dic[head]
+        
