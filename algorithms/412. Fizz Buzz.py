eeaa30312c32c21f7537bec86294eaee12427e4d
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-06 00:55:59 -0400

    Create 412. Fizz Buzz.py

---
+++ algorithms/412. Fizz Buzz.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
+        return [(not i%3)*"Fizz" + (not i%5)*"Buzz" or str(i) for i in range(1, n+1)]
