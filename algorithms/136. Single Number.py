c64ad61be2c480104bf262d3225f936816f1281b
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-11 14:28:52 -0400

    Create 136. Single Number.py

---
+++ algorithms/136. Single Number.py
@@ -0,0 +1,17 @@
+#0 ^ every number equal this number 
+# ^ twice equal it self
+
+
+# or use set and math
+
+
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
