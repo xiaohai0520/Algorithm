e88a4a076a3417d878b9bf280d19d9f5fe518e5a
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-16 00:15:59 -0400

    Update 80. Remove Duplicates from Sorted Array II.py

--- algorithms/80. Remove Duplicates from Sorted Array II.py
+++ algorithms/80. Remove Duplicates from Sorted Array II.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        for num in nums:
+            if i < 2 or num > nums[i-2]:
+                nums[i] = num
+                i += 1
+        return i
+                
