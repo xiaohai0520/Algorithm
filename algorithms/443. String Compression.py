63970d6afec8476600dd7ccdad71570e19c79efc
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-18 11:28:23 -0400

    Create 443. String Compression.py

---
+++ algorithms/443. String Compression.py
@@ -0,0 +1,20 @@
+#two pointers one point first char one to iterate
+
+class Solution:
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        left = i = 0
+        while i < len(chars):
+            char, length = chars[i], 1
+            while (i + 1) < len(chars) and char == chars[i + 1]:
+                length, i = length + 1, i + 1
+            chars[left] = char
+            if length > 1:
+                len_str = str(length)
+                chars[left + 1:left + 1 + len(len_str)] = list(len_str)
+                left += len(len_str)
+            left, i = left + 1, i + 1
+        return left   
