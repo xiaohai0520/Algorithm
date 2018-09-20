f9cdfb32ed6e5ca59011838274ab0339beb2db99
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-20 10:48:32 -0400

    Update 117. Populating Next Right Pointers in Each Node II.py

--- algorithms/117. Populating Next Right Pointers in Each Node II.py
+++ algorithms/117. Populating Next Right Pointers in Each Node II.py
@@ -1,3 +1,5 @@
+#second algorithm dummy line every level
+
 # Definition for binary tree with next pointer.
 # class TreeLinkNode:
 #     def __init__(self, x):
@@ -29,6 +31,17 @@ class Solution:
          
             n = len(queue)
                     
-                    
-                
+     #niubi a                
+   def connect(self, node):
+        tail = dummy = TreeLinkNode(0)
+        while node:
+            tail.next = node.left
+            if tail.next:
+                tail = tail.next
+            tail.next = node.right
+            if tail.next:
+                tail = tail.next
+            node = node.next
+            if not node:
+                tail = dummy
         
