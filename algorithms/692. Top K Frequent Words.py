8bbbc4c46f54d967963e732d6c43b37768a8e967
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-09-20 11:11:54 -0400

    Create 692. Top K Frequent Words.py

---
+++ algorithms/692. Top K Frequent Words.py
@@ -0,0 +1,32 @@
+#use counter to cal time and use heap to get the top k
+
+class Solution:
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        count = collections.Counter(words)
+        heap = []
+        for key, value in count.items():
+            heapq.heappush(heap, Word(value, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap).word)
+        return res[::-1]
+
+class Word:
+    def __init__(self, freq, word):
+        self.freq = freq
+        self.word = word
+    
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word > other.word
+        return self.freq < other.freq
+    
+    def __eq__(self, other):
+        return self.freq == other.freq and self.word == other.word
