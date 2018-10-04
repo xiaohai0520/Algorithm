04e4ef27e54008519b45f8d79ef08a5d605688b2
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-03 21:31:49 -0400

    Create 221. Maximal Square.py

---
+++ algorithms/221. Maximal Square.py
@@ -0,0 +1,18 @@
+#dp
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix: return 0
+        m , n = len(matrix),len(matrix[0])
+        dp = [[0 if matrix[i][j]=='0' else 1for j in xrange(n)]for i in xrange(m)]
+
+        for i in xrange(1,m):
+            for j in xrange(1,n):
+                if matrix[i][j] =='1': dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                else: dp[i][j] = 0
+
+        ans = max([max(i) for i in dp])
+        return ans ** 2
