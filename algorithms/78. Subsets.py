07f10ecc229e5ee56ef670d103969ed51b07204a
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-05 11:31:45 -0400

    Create 78. Subsets.py

---
+++ algorithms/78. Subsets.py
@@ -0,0 +1,15 @@
+#dfs
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, 0, [], res)
+        return res
+    
+    def dfs(self, nums, index, path, res):
+        res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i+1, path+[nums[i]], res)
