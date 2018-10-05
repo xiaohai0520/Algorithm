521278ea9875fc0660963f64b0345927437e2461
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-05 11:12:42 -0400

    Create 198. House Robber.py

---
+++ algorithms/198. House Robber.py
@@ -0,0 +1,17 @@
+#dp
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-2] + nums[i],dp[i-1])
+        return dp[-1]
