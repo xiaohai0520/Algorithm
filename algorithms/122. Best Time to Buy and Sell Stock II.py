95d3f2fbea4ec6673f172b458b1902c3453a97a0
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-10 10:19:04 -0400

    Update 122. Best Time to Buy and Sell Stock II.py

--- algorithms/122. Best Time to Buy and Sell Stock II.py
+++ algorithms/122. Best Time to Buy and Sell Stock II.py
@@ -9,11 +9,16 @@ class Solution:
         if not prices:
             return 0
         maxp = 0
-        start = prices[0]
-        for i in range(len(prices)):
-            if prices[i] > start:
-                maxp += (prices[i] - start)
-                start = prices[i]
-            else:
-                start = prices[i]
+        # start = prices[0]
+        # for i in range(len(prices)):
+        #     if prices[i] > start:
+        #         maxp += (prices[i] - start)
+        #         start = prices[i]
+        #     else:
+        #         start = prices[i]
+        # return maxp
+        
+        for i in range(len(prices)-1):
+            if prices[i+1] - prices[i] > 0:
+                maxp += prices[i+1] - prices[i]
         return maxp
