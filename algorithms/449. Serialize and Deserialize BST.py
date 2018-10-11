3bb7724e743380b7d599c8eba040cd150579b6d2
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-11 11:59:58 -0400

    Create 449. Serialize and Deserialize BST.py

---
+++ algorithms/449. Serialize and Deserialize BST.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        vals = []
+
+        def preOrder(node):
+            if node:
+                vals.append(node.val)
+                preOrder(node.left)
+                preOrder(node.right)
+
+        preOrder(root)
+
+        return ' '.join(map(str, vals))
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = collections.deque(int(val) for val in data.split())
+
+        def build(minVal, maxVal):
+            if vals and minVal < vals[0] < maxVal:
+                val = vals.popleft()
+                node = TreeNode(val)
+                node.left = build(minVal, val)
+                node.right = build(val, maxVal)
+                return node
+
+        return build(float('-infinity'), float('infinity'))
+
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
