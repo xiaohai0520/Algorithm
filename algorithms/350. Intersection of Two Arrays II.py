48a15b47a209b3d7b3da3a9c87edcc870644152a
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-12 23:10:23 -0400

    Create 350. Intersection of Two Arrays II.py

---
+++ algorithms/350. Intersection of Two Arrays II.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        counts = {}
+        res = []
+
+        for num in nums1:
+            counts[num] = counts.get(num, 0) + 1
+
+        for num in nums2:
+            if num in counts and counts[num] > 0:
+                res.append(num)
+                counts[num] -= 1
+
+        return res
+        
+        
+        
+        
+        
+        
+        
