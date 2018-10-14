e37585b383f1c719dc36177ddc1c936c297f33c3
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-14 09:58:51 -0400

    Create 148.Sort List.py

---
+++ algorithms/148.Sort List.py
@@ -0,0 +1,46 @@
+#merge  divid into two  and sort
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        # divide list into two parts
+        fast, slow = head.next, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        second = slow.next
+        # cut down the first part
+        slow.next = None
+        l = self.sortList(head)
+        r = self.sortList(second)
+        return self.merge(l, r)
+
+    # merge in-place without dummy node        
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        if l.val > r.val:
+            l, r = r, l
+        # get the return node "head"
+        head = pre = l
+        l = l.next
+        while l and r:
+            if l.val < r.val:
+                l = l.next
+            else:
+                pre.next,r,pre.next.next = r,r.next,pre.next
+            pre = pre.next
+        # l and r at least one is None
+        pre.next = l or r
+        return head
