bc34b4da057847fdf0839d86518e87b2378e6392
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-16 14:58:41 -0400

    Create 166.Fraction to Recurring Decimal .py

---
+++ algorithms/166.Fraction to Recurring Decimal .py
@@ -0,0 +1,32 @@
+class Solution:
+    def fractionToDecimal(self, numerator, denominator):
+        """
+        :type numerator: int
+        :type denominator: int
+        :rtype: str
+        """
+            
+        num, den = numerator, denominator
+        if not den:  # denominator is 0
+            return 
+        if not num:  # numerator is 0
+            return "0"
+        res = []
+        if (num < 0) ^ (den < 0):
+            res.append("-")  # add the sign
+        num, den = abs(num), abs(den)
+        res.append(str(num//den))
+        rmd = num % den
+        if not rmd:
+            return "".join(res)  # only has integral part
+        res.append(".")  # has frational part
+        dic = {}
+        while rmd:
+            if rmd in dic:   # the remainder recurs
+                res.insert(dic[rmd], "(")
+                res.append(")")
+                break
+            dic[rmd] = len(res) 
+            div, rmd = divmod(rmd*10, den)
+            res.append(str(div))
+        return "".join(res)
