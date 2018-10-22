6de5e57f2cd833b8f448b7b55a85ea9620e9d1fc
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-22 12:27:06 -0400

    Create 384.Shuffle an Array.py

---
+++ algorithms/384.Shuffle an Array.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+        
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        ans = self.nums[:]                     # copy list
+        for i in range(len(ans)):     # start from end
+            j = random.randrange(0, i+1)    # generate random index 
+            ans[i], ans[j] = ans[j], ans[i]    # swap
+        return ans
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
