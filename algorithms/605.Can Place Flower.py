63abf79d9d958126a9686a1032fc3e8e49f49ac8
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-23 10:07:17 -0400

    Create 605.Can Place Flower.py

---
+++ algorithms/605.Can Place Flower.py
@@ -0,0 +1,18 @@
+#greedy from start
+class Solution:
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:return True
+        f = [0] + flowerbed + [0]
+        for i in range(1,len(f)-1):
+            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+                f[i] = 1
+                n -= 1
+                if n <= 0:
+                    return True
+        return False
+                
