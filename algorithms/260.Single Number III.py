664f7befe0f56323ae8e348f5ce18437c0b88aec
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-30 12:53:42 -0400

    Create 260.Single Number III.py

---
+++ algorithms/260.Single Number III.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        xor = 0
+        for num in nums:
+            xor ^= num
+        lowbit = xor & -xor
+        a = b = 0
+        for num in nums:
+            if num & lowbit:
+                a ^= num
+            else:
+                b ^= num
+        return [a, b]
