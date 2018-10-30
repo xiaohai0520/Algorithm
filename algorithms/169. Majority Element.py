b22c3f1f72cc481db357bd2f07471eddc2594a49
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-10-30 14:01:30 -0400

    Update 169. Majority Element.py

--- algorithms/169. Majority Element.py
+++ algorithms/169. Majority Element.py
@@ -4,5 +4,15 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        return nums[len(nums)//2]
+        # nums.sort()
+        # return nums[len(nums)//2]
+    
+        time,target = 0,0
+        for num in nums:
+            if num == target:
+                time += 1
+            elif time == 0:
+                time,target = 1,num
+            else:
+                time -= 1
+        return target
