e2fb9510087a43ab59ecc945eebc2469de78de93
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-11-07 11:09:25 -0500

    Update 695. Max Area of Island.py

--- algorithms/695. Max Area of Island.py
+++ algorithms/695. Max Area of Island.py
@@ -1,25 +1,21 @@
-#dfs to calculate the areas of each island
-
-
 class Solution:
     def maxAreaOfIsland(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        m = len(grid)
-        n = len(grid[0])
-        areas = []
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] :
-                    areas.append(self.dfs(grid,i,j))
-        return max(areas) if areas else 0
+        area = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    curarea = self.dfs(grid,i,j)
+                    area = max(area,curarea)
+        return area
         
         
     def dfs(self,grid,i,j):
-        if 0<= i < len(grid) and 0<= j < len(grid[0]) and grid[i][j]:
-            grid[i][j] = 0
-            return 1+ self.dfs(grid,i-1,j) + self.dfs(grid,i+1,j) + self.dfs(grid,i,j-1) + self.dfs(grid,i,j+1)
-        else:
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0:
             return 0
+        grid[i][j] = 0
+        return 1 + self.dfs(grid,i+1,j) + self.dfs(grid,i-1,j) + self.dfs(grid,i,j+1) + self.dfs(grid,i,j-1)
+        
