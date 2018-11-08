e3e8ae96153f904890c29bcb65e520e564feb27c
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-11-08 10:04:27 -0500

    Update 50. Pow(x, n).py

--- algorithms/50. Pow(x, n).py
+++ algorithms/50. Pow(x, n).py
@@ -1,5 +1,3 @@
-#logn if %2 x*pow(x,n-1)    else  pow(x*x,n/2)
-
 class Solution:
     def myPow(self, x, n):
         """
@@ -7,12 +5,14 @@ class Solution:
         :type n: int
         :rtype: float
         """
+        if n < 0:
+            x = 1/x
+            n = -n
         if n == 0:
             return 1
-        if n < 0:
-            return self.myPow(x,-n)
-        if n%2:
-            return x * self.myPow(x, n-1)
-        else:
-            return self.myPow(x*x,n/2)
-        
+        if n == 1:
+            return x
+        if n%2 == 0:
+            return self.myPow(x*x,n//2)
+        elif n%2 == 1:
+            return self.myPow(x*x,n//2) * x
