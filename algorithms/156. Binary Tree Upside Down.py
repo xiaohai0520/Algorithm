2d0f8bbc2833be6bde97fbe0da2588aef9c960d5
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-11-14 16:03:10 -0500

    Create 156. Binary Tree Upside Down.py

---
+++ algorithms/156. Binary Tree Upside Down.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def upsideDownBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or not root.left:
+            return root
+        newRoot = self.upsideDownBinaryTree(root.left)
+        root.left.left = root.right
+        root.left.right = root
+        root.left = None
+        root.right = None
+        return newRoot
