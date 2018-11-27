82fc29652e90d84d12fd858250ad665d52abaabb
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-11-27 11:16:30 -0500

    Update 215. Kth Largest Element in an Array.py

--- algorithms/215. Kth Largest Element in an Array.py
+++ algorithms/215. Kth Largest Element in an Array.py
@@ -8,15 +8,30 @@ class Solution:
         :type k: int
         :rtype: int
         """
-        pivot = nums[0]
-        left  = [l for l in nums if l < pivot]
-        equal = [e for e in nums if e == pivot]
-        right = [r for r in nums if r > pivot]
+        #quick sort
 
-        if k <= len(right):
-            return self.findKthLargest(right, k)
-        elif (k - len(right)) <= len(equal):
-            return equal[0]
-        else:
-            return self.findKthLargest(left, k - len(right) - len(equal))
+#         pivot = nums[0]   
+#         large = [num for num in nums if num > pivot]
+#         equal = [num for num in nums if num == pivot]
+#         small = [num for num in nums if num < pivot]
+        
+#         if k <= len(large):
+#             return self.findKthLargest(large,k)
+#         elif len(large) < k <= len(equal) + len(large):
+#             return pivot
+#         else:
+#             return self.findKthLargest(small,k-len(equal)-len(large))
 
+
+        import heapq
+        min_heap = nums[:k]
+        heapq.heapify(min_heap)
+        for i in range(k,len(nums)):
+            if nums[i] > min_heap[0]:
+                heapq.heapreplace(min_heap,nums[i])
+        return min_heap[0]
+    
+    
+
+    
+        
