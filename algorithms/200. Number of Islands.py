991ba0ecbd9d91ccd60e93b13c7f2801f94866cb
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2018-12-23 10:20:58 -0500

    Update 200. Number of Islands.py

--- algorithms/200. Number of Islands.py
+++ algorithms/200. Number of Islands.py
@@ -6,21 +6,27 @@ class Solution:
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
-            return 0
+        def dfs(i,j):
+            if j != 0 and grid[i][j-1] == '1':
+                grid[i][j-1] = '2'
+                dfs(i,j-1)
+            if j != len(grid[0]) -1 and grid[i][j+1] == '1':
+                grid[i][j+1] = '2'
+                dfs(i,j+1)
+            if i != 0 and grid[i-1][j] == '1':
+                grid[i-1][j] = '2'
+                dfs(i-1,j)
+            if i != len(grid) -1 and grid[i+1][j] == '1':
+                grid[i+1][j] = '2'
+                dfs(i+1,j)
+        
         count = 0
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    self.dfs(grid,i,j)
+                    grid[i][j] = '2'
+                    
+                    dfs(i,j)
                     count += 1
         return count
-    
-    def dfs(self,grid,i,j):
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '0'
-        self.dfs(grid,i+1,j)
-        self.dfs(grid,i-1,j)
-        self.dfs(grid,i,j+1)
-        self.dfs(grid,i,j-1)
