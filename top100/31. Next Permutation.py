492a059016c433369e683fd5c8949186b3344b07
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-01-15 23:25:29 -0500

    Create 31. Next Permutation.py

---
+++ top100/31. Next Permutation.py
@@ -0,0 +1,36 @@
+class Solution:
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
+        if not nums:
+            return 
+     
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+ 
+        if i == -1:
+            self.swap(nums,0,len(nums)-1)
+            return
+        
+        j = len(nums) - 1
+        
+        while j >= 0 and nums[j] <= nums[i]:
+            j -= 1
+        nums[i],nums[j] = nums[j],nums[i]
+        self.swap(nums,i+1,len(nums)-1)
+        
+            
+            
+    def swap(self,nums,left,right):
+        while left < right:
+            nums[left], nums[right] = nums[right],nums[left]
+            left += 1
+            right -= 1
+            
+            
+            
+        
