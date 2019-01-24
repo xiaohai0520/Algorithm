137f4884119de2c4f136084bf670f66f17ed52ba
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-01-24 12:00:38 -0500

    Update 221. Maximal Square.py

--- algorithms/221. Maximal Square.py
+++ algorithms/221. Maximal Square.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         if not matrix: return 0
         m , n = len(matrix),len(matrix[0])
-        dp = [[0 if matrix[i][j]=='0' else 1for j in xrange(n)]for i in xrange(m)]
+        dp = [[0 if matrix[i][j]=='0' else 1 for j in xrange(n)]for i in xrange(m)]
 
         for i in xrange(1,m):
             for j in xrange(1,n):
