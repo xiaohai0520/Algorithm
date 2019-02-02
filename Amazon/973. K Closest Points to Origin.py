05c2d65f3b7f3858f98ba3d66936ad68b76e68f2
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-02-01 21:58:18 -0500

    Create 973. K Closest Points to Origin.py

---
+++ Amazon/973. K Closest Points to Origin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        points.sort(key=lambda x:(x[0]**2 + x[1]**2)**0.5)
+        return points[:K]
