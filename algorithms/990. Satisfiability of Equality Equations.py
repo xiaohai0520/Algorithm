613e0b3f8b3bfb0a28c63a669601baa04cb18b3f
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-03-29 14:27:02 -0400

    Create 990. Satisfiability of Equality Equations.py

---
+++ algorithms/990. Satisfiability of Equality Equations.py
@@ -0,0 +1,10 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        def find(x):
+            if x != uf[x]: uf[x] = find(uf[x])
+            return uf[x]
+        uf = {a: a for a in string.ascii_lowercase}
+        for a, e, _, b in equations:
+            if e == "=":
+                uf[find(a)] = find(b)
+        return not any(e == "!" and find(a) == find(b) for a, e, _, b in equations)
