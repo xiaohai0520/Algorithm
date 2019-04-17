23254773423d370f613f34c9b35878010c434b2a
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-17 11:18:01 -0400

    Update 1014. Best Sightseeing Pair.py

--- algorithms/1014. Best Sightseeing Pair.py
+++ algorithms/1014. Best Sightseeing Pair.py
@@ -1,3 +1,15 @@
+This is a dp problem.
+
+We want to find the maximum pair. 
+
+Record two values, cur_max_res and pre_max_val
+
+every time we need to update the cur_max_res
+
+every time also need to update the previous max value (remember to - 1 for the step)
+
+code:
+    
 class Solution:
     def maxScoreSightseeingPair(self, A: List[int]) -> int:
         res = pre = 0
