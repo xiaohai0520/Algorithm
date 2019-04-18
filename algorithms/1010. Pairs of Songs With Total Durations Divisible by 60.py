445c18cdfbd022de5fbc9596163f4100638c7ec4
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-18 11:01:29 -0400

    Update 1010. Pairs of Songs With Total Durations Divisible by 60.py

--- algorithms/1010. Pairs of Songs With Total Durations Divisible by 60.py
+++ algorithms/1010. Pairs of Songs With Total Durations Divisible by 60.py
@@ -1,16 +1,19 @@
+This is a array and math problem.
+
+It is as same as the two sum problem.
+
+every time we search the other part of the remainder, and save the remainder.
+
+The problem is to find the 0 condition.
+
+code:
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        n=len(time)
-        if n==1: return 0
-        
-        # 1 计数每个不同余数所含个数
-        counts = [0]*60
+        if not time:
+            return 0
+        dic = collections.defaultdict(int)
+        res = 0
         for t in time:
-            counts[t%60]+=1
-        
-        # 2 计数可被整除的时间对
-        count_pair = (counts[0]*(counts[0]-1) + counts[30]*(counts[30]-1))//2
-        for i in range(1,30):
-            count_pair += counts[i]*counts[60-i]
-        
-        return count_pair
+            res += dic[(60 - t % 60)%60]
+            dic[t%60] += 1
+        return res
