689d87af59f144252edaca665388388f5069ceb7
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-18 11:49:38 -0400

    Update 1008. Construct Binary Search Tree from Preorder Traversal.py

--- algorithms/1008. Construct Binary Search Tree from Preorder Traversal.py
+++ algorithms/1008. Construct Binary Search Tree from Preorder Traversal.py
@@ -1,3 +1,13 @@
+A tree problem.
+
+We think about the iteriative method.
+
+We save the node in the stack, and if we meet the right value,find the its root through pop.
+
+At last ,return the root.
+
+Code:
+    
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -7,16 +17,34 @@
 
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        if not preorder:
-            return None
-        cur = preorder.pop(0)
+#         if not preorder:
+#             return None
+#         root = TreeNode(preorder[0])
+#         l = [i for i in preorder[1:] if i < preorder[0]]
+#         r = [i for i in preorder[1:] if i > preorder[0]]
         
-        left = [i for i in preorder if i < cur] if preorder else []
-        right = [i for i in preorder if i > cur] if preorder else []
+#         root.left = self.bstFromPreorder(l)
+#         root.right = self.bstFromPreorder(r)
         
-        root = TreeNode(cur)
-        root.left = self.bstFromPreorder(left)
-        root.right = self.bstFromPreorder(right)
+#         return root
+
+
+        root = TreeNode(preorder[0])
+        stack = [root]
+        for v in preorder[1:]:
+            cur = TreeNode(v)
+            if v < stack[-1].val:
+                stack[-1].left = cur
+            else:
+                while stack and stack[-1].val < v:
+                    p = stack.pop()
+                p.right = cur
+            stack.append(cur)
         
-        return root
+        return stack[0]
+            
+            
+            
+            
+            
         
