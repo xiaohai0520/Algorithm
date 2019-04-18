c169fff4736414790761756e5c1a237c7f693212
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-18 10:36:39 -0400

    Update 1011. Capacity To Ship Packages Within D Days.py

--- algorithms/1011. Capacity To Ship Packages Within D Days.py
+++ algorithms/1011. Capacity To Ship Packages Within D Days.py
@@ -1,19 +1,35 @@
+This is a binary search problem.
+
+weight is the target, we use the binary search to try the different weight.
+
+For each weight, we just calculate the days.
+
+For binary search, the l is the lower boundry.
+
+
+code:
 class Solution:
     def shipWithinDays(self, weights: List[int], D: int) -> int:
+        if not weights:
+            return 0
         l,r = max(weights),sum(weights)
         while l < r:
-            cur = 0
-            need = 1
+            # print(l,r)
             mid = (l + r)//2
+            # print(mid)
+            days = 1
+            cur = 0
             for w in weights:
-                if cur + w > mid:
-                    need += 1
-                    cur = 0
-                cur += w
+                if cur + w <= mid:
+                    cur += w
+                else:
+                    cur = w
+                    days += 1
             
-            if need > D:
+             
+            if days > D:
                 l = mid + 1
             else:
                 r = mid
-                
+              
         return l
