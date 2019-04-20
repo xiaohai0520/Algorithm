3f992dc2823cb6e3ca8ccd9901f93c7737204f95
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-20 16:19:32 -0400

    Update 990. Satisfiability of Equality Equations.py

--- algorithms/990. Satisfiability of Equality Equations.py
+++ algorithms/990. Satisfiability of Equality Equations.py
@@ -1,6 +1,14 @@
-class Solution:
-    def equationsPossible(self, equations: List[str]) -> bool:
-        def find(x):
+union find algorithm.
+
+Make the union find structure first. 
+
+we do the union when  ==
+
+find each != in the array.
+
+
+Code：
+def find(x):
             if x != uf[x]: uf[x] = find(uf[x])
             return uf[x]
         uf = {a: a for a in string.ascii_lowercase}
@@ -8,3 +16,41 @@ class Solution:
             if e == "=":
                 uf[find(a)] = find(b)
         return not any(e == "!" and find(a) == find(b) for a, e, _, b in equations)
+    
+    
+    
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        
+        dic = {i:set([i]) for i in string.ascii_lowercase}
+        
+        
+        def union(v1,v2):
+            k1 = find(v1)
+            k2 = find(v2)
+            # print(k1,k2)
+            if k1 != k2:
+                dic[k1] = dic[k1].union(dic[k2])
+            
+            # dic[k1].update(dic[k2])
+            # print(dic[k1])
+                del dic[k2]
+
+            
+        def find(v):
+            for key,values in dic.items():
+                
+                if v in values:
+                    return key
+        
+        for e in equations:
+            if e[1] == '=' and e[0] != e[3]:
+                union(e[0],e[3])
+        for e in equations:
+            if e[1] == '!':
+                k1 = find(e[0])
+                k2 = find(e[3])
+                if k1 == k2:
+                    return False
+        return True
+        
