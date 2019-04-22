1132be5beece52a383aa5ed72e05f879e76d27d6
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-22 15:42:19 -0400

    Update 974. Subarray Sums Divisible by K.py

--- algorithms/974. Subarray Sums Divisible by K.py
+++ algorithms/974. Subarray Sums Divisible by K.py
@@ -1,11 +1,25 @@
+This problem we can see the subarray.
+
+We the subarray we need to think about the dict
+
+divide by K means remainder is 0
+
+we need to remember the previous remainder number 
+
+if we meet the same remainder, add that number is OK.
+
+
+CODE:
+    
 class Solution:
     def subarraysDivByK(self, A: List[int], K: int) -> int:
+        dic = collections.defaultdict(int)
+        dic[0] = 1
         res = 0
         pre = 0
-        re = [1] + [0] * (K - 1)
         for a in A:
             pre += a
-            r = pre%K
-            res += re[r]
-            re[r] += 1
+            re = pre%K
+            res += dic[re]
+            dic[re] += 1
         return res
