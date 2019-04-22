30b48d9b7e6af251ceaa513b958db57191f8d493
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-22 16:09:14 -0400

    Update 973. K Closest Points to Origin.py

--- algorithms/973. K Closest Points to Origin.py
+++ algorithms/973. K Closest Points to Origin.py
@@ -1,3 +1,12 @@
+This is a problem we can solve with heap.
+
+ALways heap is a min heap ,pop out the min
+
+now we can transfer it into a max heap with input is -num
+
+only use the memory of K
+
+code:
 class Solution:
     def kClosest(self, points, K):
         """
@@ -5,5 +14,19 @@ class Solution:
         :type K: int
         :rtype: List[List[int]]
         """
-        points.sort(key=lambda x:(x[0]**2 + x[1]**2)**0.5)
-        return points[:K]
+        import heapq
+        heap = []
+        i = 0
+        for point in points:
+            cur = point[0] **2 + point[1]**2
+            if i < K:
+                heapq.heappush(heap,( -cur ,point[0],point[1]))
+                i += 1
+                continue
+            if cur < -heap[0][0]:
+                heapq.heapreplace(heap,(-cur,point[0],point[1]))
+        res = []
+        for i in range(len(heap)):
+            cur = heapq.heappop(heap)
+            res.append([cur[1],cur[2]])
+        return res[::-1]
