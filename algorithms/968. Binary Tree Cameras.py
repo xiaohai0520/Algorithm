4940600d35a4f46cd69ed2c3c7b6cdd87ac10d48
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-23 10:57:36 -0400

    Update 968. Binary Tree Cameras.py

--- algorithms/968. Binary Tree Cameras.py
+++ algorithms/968. Binary Tree Cameras.py
@@ -1,3 +1,19 @@
+This is a tree problem
+
+We start from the bottom to the up.
+
+we mark leaf 0, leaf's pre 1 ; leaf's pre pre 2
+
+we only want to add camera on the 1
+
+so l,r has 2 return 0
+
+if l,r has 0 ,add 1 ,return 1
+
+if l ,r has 1, return 2
+
+code:
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
