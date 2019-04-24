b031a68660091d24787dc828694bc82c918eefd5
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-24 13:08:44 -0400

    Update 951. Flip Equivalent Binary Trees.py

--- algorithms/951. Flip Equivalent Binary Trees.py
+++ algorithms/951. Flip Equivalent Binary Trees.py
@@ -1,3 +1,10 @@
+Tree problem
+
+Try flip and not flip for each node.
+
+either is true is OK.
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -20,3 +27,24 @@ class Solution(object):
             return False
         return ((self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right))
                 or (self.flipEquiv(root1.right, root2.left) and self.flipEquiv(root1.left, root2.right)))
+    
+    
+    
+    
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+        if not root1 and not root2:
+            return True
+        elif not root1 or not root2:
+            return False
+        
+        if root1.val != root2.val:
+            return False
+        
+        if root1.left:
+            if root2.left and root1.left.val != root2.left.val or not root2.left:
+                root2.left,root2.right = root2.right,root2.left
+        else:
+            if root2.left:
+                root2.left,root2.right = root2.right,root2.left
+        
+        return self.flipEquiv(root1.left,root2.left) and self.flipEquiv(root1.right,root2.right)
