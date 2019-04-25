c6843abce3b2c07f407cad945b48c56b1b29fd96
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-25 11:30:08 -0400

    Update 945. Minimum Increment to Make Array Unique.py

--- algorithms/945. Minimum Increment to Make Array Unique.py
+++ algorithms/945. Minimum Increment to Make Array Unique.py
@@ -1,11 +1,19 @@
+This is an array problem.
+
+We sort the array first, we know the first one we do not need to change.
+
+From the second one, use update the need(n)  we want to step by step to update the n but it also depend on the next n.
+
+
+
 class Solution:
     def minIncrementForUnique(self, A: List[int]) -> int:
-        if not A or len(A) == 1:
+        if not A:
             return 0
-        
-        res = need = 0
-        for i in sorted(A):
-            res += max(need-i,0)
-            need = max(need+1,i+1)
+        A.sort()
+        n = A[0]
+        res = 0
+        for i in range(1,len(A)):
+            n = max(n+1,A[i])
+            res += n - A[i]
         return res
-        
