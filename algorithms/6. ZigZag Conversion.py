98606a9f25111e1aab5976279399ba6610c13152
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-04-30 14:22:30 -0400

    Update 6. ZigZag Conversion.py

--- algorithms/6. ZigZag Conversion.py
+++ algorithms/6. ZigZag Conversion.py
@@ -1,34 +1,17 @@
 class Solution:
-    def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
+    def convert(self, s: str, numRows: int) -> str:
         if not s:
             return ''
         if numRows == 1:
             return s
-        res = ''
-        for i in range(numRows):
-            
-            j = i
-            
-            if i != numRows - 1 and i != 0:
-                while j < len(s):
-                    res += s[j]
-                    j = j + 2 + (numRows - i - 2) * 2
-                    if j < len(s):
-                        res += s[j]
-                        j = j + 2 * (i - 1) + 2
-                    else:
-                        break
-                    
+        res = [''] * numRows
+        circle = 2*(numRows-1)
+        
+        for i,c in enumerate(s):
+            r = i % circle
+            if r <= numRows - 1:
+                res[r] += c
             else:
-                while j < len(s):
-                    res += s[j]
-                    j = j + 2 * (numRows - 2) + 2
-        return res
-                    
-
+                res[circle-r] += c
+        return ''.join(res)
                 
