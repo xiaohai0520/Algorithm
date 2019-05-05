23d522ceaebf99a1e893b1a5eaa8b223a288fe07
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-05-05 09:32:44 -0400

    Update 42. Trapping Rain Water.py

--- algorithms/42. Trapping Rain Water.py
+++ algorithms/42. Trapping Rain Water.py
@@ -18,3 +18,27 @@ class Solution(object):
                 r -= 1
             minHeight = min(height[l], height[r])
         return water
+    
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if height == []:
+            return(0)
+        water = 0
+        size = len(height)
+
+        left_max = [-1]* size
+        right_max = [-1] * size
+
+        left_max[0] = height[0]
+
+        for i in range(size):
+            left_max[i] = max(height[i], left_max[i - 1])
+
+        right_max[size - 1] = height[size - 1]
+        for i in range(size - 2, 0, -1):
+            right_max[i] = max(height[i], right_max[i + 1])
+
+        for i in range(i, size - 1):
+            water += min(left_max[i], right_max[i]) - height[i]
+
+        return water
