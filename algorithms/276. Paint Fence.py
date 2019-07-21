aad28b7fd5548a1f8937dd3e5c2941f110d7bd57
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-07-21 10:15:04 -0400

    Create 276. Paint Fence.py

---
+++ algorithms/276. Paint Fence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return k
+        same, dif = k, k*(k-1)
+        for i in range(3, n+1):
+            same, dif = dif, (same+dif)*(k-1)
+        return same + dif
