73fe099b0ebdf5868121bed5213d20a576533e01
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-08-15 17:18:06 -0400

    Update 286. Walls and Gates.py

--- algorithms/286. Walls and Gates.py
+++ algorithms/286. Walls and Gates.py
@@ -1,24 +1,44 @@
-class Solution(object):
-    def wallsAndGates(self, rooms):
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
-        :type rooms: List[List[int]]
-        :rtype: None Do not return anything, modify rooms in-place instead.
+        Do not return anything, modify rooms in-place instead.
         """
-        if not rooms:
-            return 
-        r, c= len(rooms), len(rooms[0])
-        for i in range(r):
-            for j in range(c):
-                if rooms[i][j] == 0:
-                    queue = collections.deque([])
-                    queue.append((i+1, j, 1)); queue.append((i-1, j, 1))
-                    queue.append((i, j+1, 1)); queue.append((i, j-1, 1))
-                    visited = set()
-                    while queue:
-                        x, y, val = queue.popleft()
-                        if x < 0 or x >= r or y < 0 or y >= c or rooms[x][y] in [0, -1] or (x, y) in visited:
-                            continue
-                        visited.add((x, y))
-                        rooms[x][y] = min(rooms[x][y], val)
-                        queue.append((x+1, y, val+1)); queue.append((x-1, y, val+1))
-                        queue.append((x, y+1, val+1)); queue.append((x, y-1, val+1))
+        q = collections.deque([(i, j, 0) for i, row in enumerate(rooms) for j, col in enumerate(row) if not col])
+        print(q)
+        visited = set()
+        while q:
+            
+            x,y,val = q.popleft()
+            
+            for i,j in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+                if i<0 or j < 0 or i >= len(rooms) or j >= len(rooms[0]) or rooms[i][j] in [0,-1] or (i,j) in visited:
+                    continue
+                print((i,j))
+                rooms[i][j] = min(rooms[i][j],val+1)
+                visited.add((i,j))
+                q.append((i,j,val+1))
+        
+        
+        
+#         for i in range(len(rooms)):
+#             for j in range(len(rooms[0])):
+#                 if rooms[i][j] == 0:
+#                     queue = collections.deque([])
+#                     queue.append((i+1,j,1))
+#                     queue.append((i-1,j,1))
+#                     queue.append((i,j+1,1))
+#                     queue.append((i,j-1,1))
+#                     visited = set()
+                    
+#                     while queue:
+#                         a,b,val = queue.popleft()
+#                         if a < 0 or b < 0 or a >= len(rooms) or b >= len(rooms[0]) or rooms[a][b] in [0,-1] or (a,b) in visited:
+#                             continue
+#                         rooms[a][b] = min(rooms[a][b],val)
+#                         visited.add((a,b))
+#                         queue.append((a+1,b,val+1))
+#                         queue.append((a-1,b,val+1))
+#                         queue.append((a,b+1,val+1))
+#                         queue.append((a,b-1,val+1))
+                        
+                    
