40354d60c7fd09b327a7b4ecbf1551b2206dff2c
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-08-15 12:30:06 -0400

    Update 282. Expression Add Operators.py

--- algorithms/282. Expression Add Operators.py
+++ algorithms/282. Expression Add Operators.py
@@ -22,3 +22,26 @@ class Solution(object):
                 self.dfs(num[i:], temp + "+" + val, cur+int(val), int(val), res)
                 self.dfs(num[i:], temp + "-" + val, cur-int(val), -int(val), res)
                 self.dfs(num[i:], temp + "*" + val, cur-last+last*int(val), last*int(val), res)
+                
+                
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        results = []
+        self.helper(num, 0, target, 0, 0, "", results)
+        return results
+    
+    def helper(self, string, start, target, sum_so_far, last, path, results):
+        if start == len(string) and sum_so_far == target:
+            results.append(path)
+        
+        for end in range(start+1, len(string)+1):
+            sub_string = string[start:end]
+            if len(sub_string) > 1 and sub_string[0] == '0':
+                break
+            cur = int(sub_string)
+            if start == 0:
+                self.helper(string, end, target, sum_so_far + cur, cur, path + sub_string, results)
+            else:
+                self.helper(string, end, target, sum_so_far + cur, cur, path + "+" + sub_string, results)
+                self.helper(string, end, target, sum_so_far - cur, -cur, path + "-" + sub_string, results)
+                self.helper(string, end, target, sum_so_far - last + cur * last, cur * last, path + "*" + sub_string, results)
