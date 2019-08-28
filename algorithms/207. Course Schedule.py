18bd54a229e0d535578a2b8b63b77112f8b36bb1
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-08-28 09:05:11 -0400

    Update 207. Course Schedule.py

--- algorithms/207. Course Schedule.py
+++ algorithms/207. Course Schedule.py
@@ -1,19 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {n:[] for n in range(numCourses)} 
-        for i,j in prerequisites:
-            graph[i].append(j)
-            
-        for n in range(numCourses):
-            stack = graph[n]
-            visited = set()
-            while stack:
-                course = stack.pop()
-                visited.add(course)
-                if course == n:
-                    return False
-                for c in graph[course]:
-                    if c not in visited:
-                        stack.append(c)
-        return True
+        from collections import deque
+        ind = [[] for _ in range(numCourses)]  # indegree
+        oud = [0] * numCourses  # outdegree
+        for p in prerequisites:
+            oud[p[0]] += 1
+            ind[p[1]].append(p[0])
+        dq = deque()
+        for i in range(numCourses):
+            if oud[i] == 0:
+                dq.append(i)
+        k = 0
+        while dq:
+            x = dq.popleft()
+            k += 1
+            for i in ind[x]:
+                oud[i] -= 1
+                if oud[i] == 0:
+                    dq.append(i)
+        return k == numCourses
             
