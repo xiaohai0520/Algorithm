62519358f7186d9f29c1f418d655a1a3a60ff620
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-08-30 17:47:33 -0400

    Update 220. Contains Duplicate III.py

--- algorithms/220. Contains Duplicate III.py
+++ algorithms/220. Contains Duplicate III.py
@@ -1,11 +1,21 @@
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
-        if t==0 and len(nums)== len(set(nums)): 
-            return False
-        for i in range(len(nums)):
-            for j in range(i+1, i+k+1):
-                if j>=len(nums):
-                    break
-                if nums[i]-nums[j]>=-t and nums[i]-nums[j]<=t:
+       
+        # Bucket sort. Each bucket has size of t. For each number, the possible
+    # candidate can only be in the same bucket or the two buckets besides.
+    # Keep as many as k buckets to ensure that the difference is at most k.
+        buckets = {}
+        for i, v in enumerate(nums):
+            # t == 0 is a special case where we only have to check the bucket
+            # that v is in.
+            bucketNum, offset = (v // t, 1) if t else (v, 0)
+            for idx in range(bucketNum - offset, bucketNum + offset + 1):
+                if idx in buckets and abs(buckets[idx] - nums[i]) <= t:
                     return True
+
+            buckets[bucketNum] = nums[i]
+            if len(buckets) > k:
+                # Remove the bucket which is too far away. Beware of zero t.
+                del buckets[nums[i - k] // t if t else nums[i - k]]
+
         return False
