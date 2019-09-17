ca6dad31e6c9fe5f4d711240723a325a7f76216f
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-09-17 13:56:45 -0400

    Create 541. Reverse String II.py

---
+++ algorithms/541. Reverse String II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        
+        
+        if len(s) < k:
+            return s[::-1]
+        elif k <= len(s) < 2*k:
+            return s[:k][::-1] + s[k:]
+        else:
+            return s[:k][::-1] + s[k:2*k] + self.reverseStr(s[2*k:],k)
