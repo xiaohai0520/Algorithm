508d95722f45da3766d255c0cc3eaa13a31c30b7
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-10-17 14:32:57 -0400

    Update 445. Add Two Numbers II.py

--- algorithms/445. Add Two Numbers II.py
+++ algorithms/445. Add Two Numbers II.py
@@ -4,6 +4,92 @@
 #         self.val = x
 #         self.next = None
 
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur1 = l1
+        cur2 = l2
+        len1 = 0
+        len2 = 0
+        while cur1:
+            len1 += 1
+            cur1 = cur1.next
+        while cur2:
+            len2 += 1
+            cur2 = cur2.next
+        
+        
+        def helper(l1,l2,len1,len2):
+            
+            if len1 == 1 and len2 == 1:
+                val = l1.val + l2.val
+                flag,val = divmod(val,10)
+                return ListNode(val),flag
+            
+            val = 0
+            if len2 > len1:
+                l1,l2 = l2,l1
+                len1,len2 = len2,len1
+            v1 = v2 = 0
+            
+            if len1 == len2:
+                v1 = l1.val
+                v2 = l2.val
+                cur,flag = helper(l1.next,l2.next,len1-1,len2-1)
+            else:
+                v1 = l1.val
+                v2 = 0
+                cur,flag = helper(l1.next,l2,len1-1,len2)
+               
+            val = v1 +v2 + flag
+            flag,val = divmod(val,10)
+            new = ListNode(val)
+            new.next = cur
+            return new,flag
+        
+        res,flag = helper(l1,l2,len1,len2) 
+        if flag:
+            cur = ListNode(flag)
+            cur.next = res
+            return cur
+        return res
+                
+        
+        # def helper(l1,l2,len1,len2):
+        #     if not l1 and not l2:
+        #         return ListNode(0)
+
+        #     val = 0
+        #     if len1 == len2:
+        #         next = helper(l1.next,l2.next,len1-1,len2-1)
+        #         val = l1.val + l2.val + next.val
+        #     else:
+        #         next = helper(l1.next,l2,len1-1,len2)
+        #         val = l1.val + next.val
+        #     next.val = val%10
+        #     val //= 10
+        #     new = ListNode(val)
+        #     new.next = next
+        #     return new
+        # res = helper(l1,l2,len1,len2)
+        # return res.next if res.val == 0 else res
+            
+
+
+
+
+
+
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 #use stack from back to front
 
 
