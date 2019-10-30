20715cfd627ac7c30722417b1c9bd78685244837
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-10-29 21:30:51 -0400

    Update 1. two sum.py

--- algorithms/1. two sum.py
+++ algorithms/1. two sum.py
@@ -1,4 +1,8 @@
-# use enumerate to save index and difference in the dic
+
+# 1.两个for循环 去尝试每一种组合，如果和为目标值，即可返回， 时间复杂度 n2.
+# 2.优化，空间换时间，使用字典存储遍历过得值和index， 如果目标值与当前值之差存在于字典中，说明存在某种组合满足目标值， 时间复杂度n。
+
+
 
 class Solution:
     def twoSum(self, nums, target):
@@ -7,12 +11,7 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-#         dic = {}
-#         for i in range(len(nums)):
-#             if nums[i] in dic:
-#                 return [dic[nums[i]], i]
-#             else:
-#                 dic[target - nums[i]] = i
+
         dic = {}        
         for i,num in enumerate(nums):
             if num in dic:
