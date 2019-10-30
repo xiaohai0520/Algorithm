a81a7c098f6c91a99db7f4d22dfacee98bb6adee
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-10-29 22:06:07 -0400

    Update 2. Add Two Numbers.py

--- algorithms/2. Add Two Numbers.py
+++ algorithms/2. Add Two Numbers.py
@@ -1,3 +1,8 @@
+# 两个链表，取一个dummy node 作为结果的head， 然后去逐渐处理两个链表的每一位
+# 如果和超过10，则使用flag 记为1并且加入下一位， 如果两个链表都用完了，flag还是1， 注意再填一位
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -5,16 +10,10 @@
 #         self.next = None
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        dummy = ListNode(0)
-        cur = dummy
-        curnum = add = 0
-        while l1 or l2:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        flag = 0
+        dummy = cur = ListNode(0)
+        while l1 or l2 or flag:
             v1 = v2 = 0
             if l1:
                 v1 = l1.val
@@ -22,11 +21,7 @@ class Solution:
             if l2:
                 v2 = l2.val
                 l2 = l2.next
-            add,curnum = divmod(v1+v2+add,10)
-            
-            cur.next = ListNode(curnum)
+            flag,val = divmod(v1+v2+flag,10)
+            cur.next = ListNode(val)
             cur = cur.next
-        if add:
-            cur.next = ListNode(add)
         return dummy.next
-            
