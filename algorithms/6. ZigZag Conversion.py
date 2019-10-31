548a3ac048a38ed6c17c874ec9c2a2c9dd0e1fcd
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-10-30 21:21:20 -0400

    Update 6. ZigZag Conversion.py

--- algorithms/6. ZigZag Conversion.py
+++ algorithms/6. ZigZag Conversion.py
@@ -1,3 +1,9 @@
+# zigzag 分割字符串，
+# 我们考虑每 2* 行数 - 2 个长度为一个循环， 其中前一部分为正序，后一部分为倒叙。
+# 建立 长度为 行数的数组，然后遍历整个字符串， 求每个字符 应该在的index,  放入相应的index中。
+# 最后将数组中所有的字符串加在一起。
+
+
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if not s:
