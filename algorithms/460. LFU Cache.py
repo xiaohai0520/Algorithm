c26a37ad1ea5721d2e6a3f13a65abeb598956a4f
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-11-04 20:05:26 -0500

    Update 460. LFU Cache.py

--- algorithms/460. LFU Cache.py
+++ algorithms/460. LFU Cache.py
@@ -1,3 +1,53 @@
+
+# OrderedDict in python is implemented by a dict+linkedList, which is essentially a LRU cache.
+# "keyfreq" dict represents a normal dict that maps one key to one frequency.
+# "freqkeys" dict represents a dict that maps one freq to many keys, and these "many keys" are stored in OrderedDict.
+# With this two dicts, given one key, we can find its frequency, and with its frequency, we can find all other keys of the same frequency.
+# When there are many items of same frequency, the OrderedDict in freqkeys dict correctly records the item order in LRC fashion where the first item will be the one to pop out.
+
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.capacity=capacity
+        self.minfreq=None
+        self.keyfreq={}
+        self.freqkeys=collections.defaultdict(collections.OrderedDict)
+
+    def get(self, key: int) -> int:
+        if key not in self.keyfreq:
+            return -1
+        freq=self.keyfreq[key]
+        val=self.freqkeys[freq][key]
+        del self.freqkeys[freq][key]
+        if not self.freqkeys[freq]:
+            if freq==self.minfreq:
+                self.minfreq+=1
+            del self.freqkeys[freq]
+        self.keyfreq[key]=freq+1
+        self.freqkeys[freq+1][key]=val
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity<=0:
+            return
+        if key in self.keyfreq:
+            freq=self.keyfreq[key]
+            self.freqkeys[freq][key]=value
+            self.get(key)
+            return
+        if self.capacity==len(self.keyfreq):
+            delkey,delval=self.freqkeys[self.minfreq].popitem(last=False)
+            del self.keyfreq[delkey]
+        self.keyfreq[key]=1
+        self.freqkeys[1][key]=value
+        self.minfreq=1
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+
 #want to say nothing
 
 LAST, NEXT, KEY, VAL, FREQ = 0, 1, 2, 3, 4 
