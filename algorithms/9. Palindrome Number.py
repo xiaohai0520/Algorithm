b1444da25d85bc5ce782c1ec48642726e33fd4a5
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-11-21 22:55:28 -0500

    Update 9. Palindrome Number.py

--- algorithms/9. Palindrome Number.py
+++ algorithms/9. Palindrome Number.py
@@ -1,5 +1,7 @@
 # tostring or use // and % from last digit to first one
-
+# 这道题基本思想是考数字的变换，如果数字小于0， 那一定不是palindrome。
+# 然后将数字进行 翻转， 每次除以10， 用余数去构造翻转后的数， divmod  方法可以同时获得两个部分。
+# 最后将结果进行比较就可以， 如果相等 则说明是palindrome。
 
 
 class Solution:
