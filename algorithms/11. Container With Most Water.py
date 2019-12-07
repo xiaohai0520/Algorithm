c7df123c028f6b9f95d9746c84315875c0025287
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-12-07 09:59:52 -0500

    Update 11. Container With Most Water.py

--- algorithms/11. Container With Most Water.py
+++ algorithms/11. Container With Most Water.py
@@ -1,4 +1,8 @@
-# two pointers from two sides, always try to find the bigger of the lower one
+这里需要定义i和j两个指针分别指向数组的左右两端，
+然后两个指针向中间搜索，每移动一次算一个值和结果比较取较大的，
+容器装水量的算法是找出左右两个边缘中较小的那个乘以两边缘的距离，
+
+
 class Solution:
     def maxArea(self, height):
         """
