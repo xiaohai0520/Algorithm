42b832782ccb47b67eed6eec59c0ea92142410fd
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-12-10 18:39:45 -0500

    Update 14. Longest Common Prefix.py

--- algorithms/14. Longest Common Prefix.py
+++ algorithms/14. Longest Common Prefix.py
@@ -1,4 +1,8 @@
-# try every char
+先从所有的字符串里找到最短的
+然后用最短的第一位开始，去对比所有字符串的每一位，如果碰见不相同的情况，则可以返回当前获得的长度，
+当遍历完最短字符串后，可以将其直接返回
+
+也可以用divide conquer 的方法来解决，分成两段，然后求出每段的最长prefix，然后比较两个
 
 class Solution:
     def longestCommonPrefix(self, strs):
