27dcf7396af442d41c9da59b50106191e8892e7c
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-12-11 08:28:45 -0500

    Update 21. Merge Two Sorted Lists.py

--- algorithms/21. Merge Two Sorted Lists.py
+++ algorithms/21. Merge Two Sorted Lists.py
@@ -1,3 +1,11 @@
+合并链表
+
+使用dummy 建立一个虚拟的头
+
+然后对两个链表进行比较，将小的一个链到头的下一个，然后小的这个链表后移，继续比较
+
+直到其中某一个链表到最后一个，最后讲剩余的链表加到尾部
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
