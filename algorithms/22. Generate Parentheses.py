07957ab527680c86f8f5f87bd37c18e1e76f2cac
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2019-12-11 08:46:37 -0500

    Update 22. Generate Parentheses.py

--- algorithms/22. Generate Parentheses.py
+++ algorithms/22. Generate Parentheses.py
@@ -1,23 +1,21 @@
-#use backtracking and set the condition not valid
-#dfs
+backtracking
+利用递归尝试每一个组和，
+直到左边值或者右边值没有了 或者左边比右边大，（ 不可以比） 剩的要少，
+时间复杂度 2^n
 
 class Solution:
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n <= 0:
+            return []
         res = []
-        self.helper(n,n,'',res)
+        
+        def dfs(l,r,path):
+            if l == 0 and r == 0:
+                res.append(path)
+                return
+            if l > r or l < 0 or r < 0:
+                return 
+            dfs(l-1,r,path+'(')
+            dfs(l,r-1,path+')')
+        dfs(n,n,'')
         return res
-        
-        
-    def helper(self,l,r,path,res):
-        if l == 0 and r == 0:
-            res.append(path)
-            return
-        if l < 0 or r < 0 or l > r:
-            return 
-        self.helper(l-1,r,path+'(',res)
-        self.helper(l,r-1,path+')',res)
