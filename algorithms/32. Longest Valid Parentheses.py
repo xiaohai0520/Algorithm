77fb2e01d4dd59403a4ff9573180d6eee14ad123
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-01-13 08:26:11 -0500

    Update 32. Longest Valid Parentheses.py

--- algorithms/32. Longest Valid Parentheses.py
+++ algorithms/32. Longest Valid Parentheses.py
@@ -1,4 +1,10 @@
-#use stack
+一般对称的问题，可以使用stack来解决问题。
+初始化一个start index, 作为左边界
+依次遍历字符串，如果为左括号，讲index放入stack,
+如果是右括号，分析情况，如果此时stack是空，说明从这位以及之前的都不能是对称的，更新start,
+
+如果不为空，可以和stack中的左括号组成一对，那么我们要讲stack中的左括号pop出一个，
+这个时候如果stack空，更新长度，以start为左边界，如果不为空，以stack中最后一个左括号的index为左边界
 
 class Solution:
     def longestValidParentheses(self, s):
