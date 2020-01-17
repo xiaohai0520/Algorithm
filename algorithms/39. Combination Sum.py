3e06c626db267dd49f44cb41f13c61867ff5ee53
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-01-17 09:31:33 -0500

    Update 39. Combination Sum.py

--- algorithms/39. Combination Sum.py
+++ algorithms/39. Combination Sum.py
@@ -1,22 +1,24 @@
-#dfs
+每个数字可以重复使用，并且没有重复的数字，
+我们使用dfs去尝试每种组合，并记录当前的和，
+如果和大于目标值，直接返回上一步
+如果等于，加入结果集
+小于的话，从当前数字继续添加进path
 
 class Solution:
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        res = []
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if not candidates:
+            return []
         candidates.sort()
-        self.dfs(candidates, target, 0,0, [], res)
-        return res
-
-    def dfs(self, nums, target, cursum,index, path, res):
+        res = []
         
-        if cursum > target:
-            return
-        if cursum == target:
-            res.append(path)
-        for i in range(index,len(nums)):
-            self.dfs(nums,target,cursum + nums[i] , i, path + [nums[i]],res)
+        def dfs(cur,path,start):
+            if cur > target:
+                return
+            if cur == target:
+                res.append(path)
+                return
+            for i in range(start,len(candidates)):
+                dfs(cur+candidates[i],path+[candidates[i]],i)
+        
+        dfs(0,[],0)
+        return res
