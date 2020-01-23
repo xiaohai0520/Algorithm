ca488aa536af181ac3cf53847ec420a5ba871195
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-01-23 09:28:08 -0500

    Update 50. Pow(x, n).py

--- algorithms/50. Pow(x, n).py
+++ algorithms/50. Pow(x, n).py
@@ -1,18 +1,12 @@
 class Solution:
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        if n < 0:
-            x = 1/x
-            n = -n
+    def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
         if n == 1:
             return x
+        if n == -1:
+            return 1/x
         if n%2 == 0:
             return self.myPow(x*x,n//2)
-        elif n%2 == 1:
+        else:
             return self.myPow(x*x,n//2) * x
