148f98c18a81f9b22adbbbbf6844ea083c42a682
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-01-29 10:48:57 -0500

    Update 59. Spiral Matrix II.py

--- algorithms/59. Spiral Matrix II.py
+++ algorithms/59. Spiral Matrix II.py
@@ -1,37 +1,38 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        res = [[0 for _ in range(n)] for _ in range(n)] 
-        up = 0
-        down = n-1
-        left = 0
-        right = n-1
-        val = 1
-        while 1:
-            for i in range(left,right+1):
-                res[up][i] = val
-                val += 1
-            up += 1
-            if up > down:
-                break
+        upper_i, upper_j, lower_i, lower_j,  = 0, n - 1, n - 1, 0 
+        num = 1
+        
+        # create n*n matrix
+        matrix = [[0 for _ in range( n )] for _ in range( n )]
+        
+        while num <= n*n:
+            for j in range( lower_j, upper_j + 1, 1 ):
+                # assign num across current most-top row
+                matrix[lower_j][j] = num
+                num += 1
+            # increment to avoid overwriting current most-top row
+            upper_i += 1
 
-            for i in range(up,down+1):
-                res[i][right] = val
-                val += 1
-            right -= 1
-            if right < left:
-                break
-                
-            for i in range(right,left-1,-1):
-                res[down][i] = val
-                val += 1
-            down -= 1
-            if down < up:
-                break
-            for i in range(down,up-1,-1):
-                res[i][left] = val
-                val += 1
-            left += 1
-            if left > right:
-                break
-        return res
-                
+            for i in range( upper_i, lower_i + 1, 1 ):
+                # assign num across current most-right column
+                matrix[i][upper_j] = num
+                num += 1
+            # decrement to avoid overwriting current most-right row
+            upper_j -= 1
+            
+            for j in range( upper_j, lower_j - 1, -1 ):
+                # assign num across current most-bottom row
+                matrix[lower_i][j] = num
+                num += 1
+            # decrement to avoid overwriting current most-bottom row
+            lower_i -= 1
+            
+            for i in range( lower_i, upper_i - 1, -1 ):
+                # assign num across current most-left column
+                matrix[i][lower_j] = num
+                num += 1
+            # increment to avoid overwriting current most-left column
+            lower_j += 1 
+            
+        return matrix
