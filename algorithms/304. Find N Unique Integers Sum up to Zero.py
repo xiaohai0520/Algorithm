98bb98f78ddbe40ab700ef82d7dc57cc142527eb
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-02-17 11:29:35 -0500

    Delete 304. Find N Unique Integers Sum up to Zero.py

--- algorithms/304. Find N Unique Integers Sum up to Zero.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sumZero(self, n: int) -> List[int]:
-        res = []
-        if n%2 == 1:
-            res.append(0)
-        for i in range(1,n//2+1):
-            res.append(i)
-            res.append(-i)
-        return res
