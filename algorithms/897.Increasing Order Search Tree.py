22e59a3045c39a0d77abf8944daed45225f27f51
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-05-08 08:42:18 -0400

    Update 897.Increasing Order Search Tree.py

--- algorithms/897.Increasing Order Search Tree.py
+++ algorithms/897.Increasing Order Search Tree.py
@@ -1,3 +1,40 @@
+## 897. Increasing Order Search Tree
+
+
+### 题目分析
+一颗BST，我们要把它变成一棵连续递增的树，每棵树只有right child。
+
+### 解法
+我们只要inorder这棵BST就可以让所有的点递增。
+
+### 代码
+```
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        
+        dummy = self.node = TreeNode(0)
+        
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            self.node.right = TreeNode(root.val)
+            self.node = self.node.right
+            dfs(root.right)
+        dfs(root)
+        return dummy.right
+        
+```
+
+另一种dfs的代码
+```
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -11,24 +48,6 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-#         if not root:
-#             return 
-        
-#         res = []
-#         stack = []
-#         while stack or root:
-#             while root:
-#                 stack.append(root)
-#                 root = root.left
-#             root = stack.pop()
-#             res.append(root.val)
-#             root = root.right
-#         res = res[::-1]
-#         Dummy = cur = TreeNode(0)
-#         while res:
-#             cur.right = TreeNode(res.pop())
-#             cur = cur.right
-#         return Dummy.right
         return self.helper(root,None)
         
     def helper(self,root,tail):
@@ -38,4 +57,4 @@ class Solution(object):
         root.left = None
         root.right = self.helper(root.right,tail)
         return res
-        
+ ```       
