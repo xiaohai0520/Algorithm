07a9ca568caa77bf0cfc1fb072af241828fc622e
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-06-01 20:18:13 -0400

    Update 114. Flatten Binary Tree to Linked List.py

--- algorithms/114. Flatten Binary Tree to Linked List.py
+++ algorithms/114. Flatten Binary Tree to Linked List.py
@@ -1,39 +1,49 @@
+## 114. Flatten Binary Tree to Linked List
+
+### 题目分析
+flat一棵二叉树。
+
+要求是不招用额外空间。
+
+
+### 解法
+
+思路是left flatten之后接在root右侧，然后right接在left flatten后面。
+
+那么我们先处理right,然后处理left,然后接在一起。
+
+因为我们一直要连接到之前的一个node,用一个全局变量来记录之前处理的node。
+
+时间复杂度O(n)
+
+空间复杂度O(1)
+
+### 代码
+```
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
+class Solution:
+    
     def __init__(self):
         self.prev = None
-        
-    def flatten(self, root):
+    
+    def flatten(self, root: TreeNode) -> None:
         """
-        :type root: TreeNode
-        :rtype: void Do not return anything, modify root in-place instead.
+        Do not return anything, modify root in-place instead.
         """
-        # if not root:
-        #     return
-        # root1 = root
-        # stack = [root1]
-        # while stack:
-        #     cur = stack.pop()
-        #     if cur is not root:
-        #         root1.right = cur
-        #         root1.left = None
-        #         root1 = root1.right
-        #     if cur.right:
-        #         stack.append(cur.right)
-        #     if cur.left:
-        #         stack.append(cur.left)
-                
         if not root:
             return None
         self.flatten(root.right)
         self.flatten(root.left)
+        
 
         root.right = self.prev
         root.left = None
         self.prev = root
+
+```
